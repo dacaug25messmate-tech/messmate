fe6361173984bf4a78804d4e5519ba11fca63408
gayatriPatil <gayatriPatil9808@gmail.com>
2026-01-23 16:19:09 +0530

    Login Validation Services Updated

--- backend/src/main/java/com/example/services/LoginService.java
+++ backend/src/main/java/com/example/services/LoginService.java
@@ -10,30 +10,29 @@ import com.example.repository.UserRepository;
 
 @Service
 public class LoginService {
-	
-	@Autowired
-	UserRepository urepo;
-	
-	public LoginResponse login(LoginRequest request){
-		User user = urepo.findByUserName(request.getUserName());
+    
+    @Autowired
+    UserRepository urepo;
+    
+    public LoginResponse login(LoginRequest request) {
+        User user = urepo.findByUserName(request.getUserName());
 
-	    LoginResponse response = new LoginResponse();
+        LoginResponse response = new LoginResponse();
 
-	    if (user == null) {
-	        response.setStatus("USER_NOT_FOUND");
-	        return response;
-	    }
+        if (user == null) {
+            response.setStatus("USER_NOT_FOUND");
+            return response;
+        }
 
-	    if (user.getPassword().equals(request.getPassword())) {
-	        response.setStatus("SUCCESS");
-	        response.setUid(user.getUserid());
-	        response.setUname(user.getUserName());	       
-	        response.setRole(user.getRoleId().getRoleName()); 
-	        return response;
-	    }
+        if (user.getPassword().equals(request.getPassword())) {
+            response.setStatus("SUCCESS");
+            response.setUid(user.getUserid());
+            response.setUname(user.getUserName());           
+            response.setRole(user.getRoleId().getRoleName()); 
+            return response;
+        }
 
-	    response.setStatus("INVALID_PASSWORD");
-	    return response;
-		
-	}
+        response.setStatus("INVALID_PASSWORD");
+        return response;
+    }
 }
