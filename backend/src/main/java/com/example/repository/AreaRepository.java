5a1d3565b6b33f4fccd60b2e237a0f0679c7ece9
shruti <patilshrutia473@gmail.com>
2026-01-23 12:57:06 +0530

    Update AreaRepository

--- backend/src/main/java/com/example/repository/AreaRepository.java
+++ backend/src/main/java/com/example/repository/AreaRepository.java
@@ -1,9 +1,11 @@
 package com.example.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.entities.Area;
 
 public interface AreaRepository extends JpaRepository<Area, Integer> {
-
+	List<Area> findByCityId_CityId(Integer cityId);
 }
